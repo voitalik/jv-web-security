d7be6d78ea62f2c49f74e69f1397048eab4192f6
Roman Voitalik <voitalik1@gmail.com>
2022-09-30 11:00:13 +0300

    deploy to heroku

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/taxi";
+    private static final String URL = "jdbc:mysql://34.116.245.5/taxi?useSSL=false";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "111111";
+    private static final String PASSWORD = "fl*z%_hp~GRxSz5k";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
